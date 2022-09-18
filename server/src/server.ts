31cb8d1dbf5d18600a0d288c606976ea462bc289
Ritesh Khore <riteshkhore@gmail.com>
2022-09-18 16:48:00 +0530

    deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,9 +26,9 @@ const PORT = process.env.PORT || 5000;
 
 console.log(`Path  is ${path.join("./uploads")} ✅✅✅`);
 
-console.log(existsSync(path.join("./uploads")));
+console.log(existsSync(path.join(__dirname, "uploads")));
 
-app.use("/uploads", express.static(path.join("uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json({ limit: "10mb" }));
 
