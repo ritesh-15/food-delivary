4dbbef2ec8ef13e83f4aa0a3234dc049c6eddbe4
Ritesh Khore <riteshkhore@gmail.com>
2022-09-18 11:53:51 +0530

    deployment

--- server/src/middlewares/upload-middleware.ts
+++ server/src/middlewares/upload-middleware.ts
@@ -4,7 +4,8 @@ import crypto from "crypto";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    path.join(__dirname, "uploads");
+    console.log("Path 👇👇👇");
+    console.log(path.join(__dirname, "uploads"));
     cb(null, path.join(__dirname, "../uploads"));
   },
   filename: (req, file, cb) => {
